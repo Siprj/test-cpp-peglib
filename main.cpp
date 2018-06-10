01b6824f803cc1f0d065b3c219ccedac0fb3b7b9
Jan Sipr <sipr.jan@gmail.com>
2018-06-10 23:54:11 +0200

    WIP

--- main.cpp
+++ main.cpp
@@ -18,12 +18,61 @@ struct Section {
     std::string type;
 };
 
-struct PropertyVariable {
-    PropertyVariable(std::string name, std::string value): name(name), value(value) {}
+struct SymbolScope;
 
-    std::string name;
-    std::string value;
-};
+auto syntax = R"(
+    CONFIG <- INCLUDE* SECTION*
+    INCLUDE <- _ '#include' _ VALUE __
+    SECTION <- SECTION_DEFINITION SECTION_VARIABLE*
+    SECTION_DEFINITION <- _ '[' _ SYMBOL_NAME ':' _ SYMBOL_NAME ']' _ COMMENT? __
+    SECTION_VARIABLE <- _ SYMBOL_NAME '=' VALUE __
+    SYMBOL_NAME <- [a-zA-Z0-9._]+ _
+    VALUE <- (![\n\r# ].)*
+    COMMENT <- '#' (![\r\n].)*
+    ~_ <- [ \t]*
+    ~__ <- ![a-zA-Z_] [ \t\r\n]*
+)";
+
+inline constexpr long unsigned int val(const char *str, long unsigned int h = 0) {
+    return !str[h] ? 5381 : (str2tag(str, h + 1) * 33) ^ static_cast<unsigned char>(str[h]);
+}
+
+inline constexpr long unsigned int tailedRec(const char *str, long unsigned int h = 0)
+{
+    return !str[h] ? 5381 : (str2tag(str, h + 1) * 33) ^ static_cast<unsigned char>(str[h]);
+}
+
+inline constexpr long unsigned int operator "" _(const char *str, long unsigned int h = 0)
+{
+    return tailedRec(str);
+}
+
+void pokus(const shared_ptr<Ast> ast)
+{
+    switch (ast->tag)
+    {
+    case "CONFIG"_:
+        break;
+    case "INCLUDE"_:
+        break;
+    case "SECTION"_:
+        break;
+    case "SECTION_DEFINITION"_:
+        break;
+    case "SECTION_VARIABLE"_:
+        break;
+    case "SYMBOL_NAME"_:
+        break;
+    case "VALUE"_:
+        break;
+    case "COMMENT"_:
+        break;
+    default:
+
+        std::cout<<"(Default) tag number: "<<ast->tag<<" and token: "<<ast->token<<" name: "<<ast->name<<"\n";
+        break;
+    }
+}
 
 string format_error_message
     ( const string& path
@@ -39,69 +88,24 @@ string format_error_message
 
 int main()
 {
-    auto syntax = R"(
-        CONFIG <- INCLUDE* SECTION*
-        INCLUDE <- _ '#include' _ VALUE __
-        SECTION <- SECTION_DEFINITION SECTION_VARIABLE*
-        SECTION_DEFINITION <- _ '[' _ SYMBOL_NAME ':' _ SYMBOL_NAME ']' _ COMMENT? __
-        SECTION_VARIABLE <- _ SYMBOL_NAME '=' VALUE __
-        SYMBOL_NAME <- [a-zA-Z0-9._]+ _
-        VALUE <- (![\n\r# ].)*
-        COMMENT <- '#' (![\r\n].)*
-        ~_ <- [ \t]*
-        ~__ <- ![a-zA-Z_] [ \t\r\n]*
-    )";
 
     peg::parser parser(syntax);
     parser.log = [&](size_t ln, size_t col, const string& msg) {
             cerr << format_error_message("TODO: file", ln, col, msg) << endl;
         };
-    parser["SYMBOL_NAME"] = [](const peg::SemanticValues &sv)
-    {
-        std::cout<<"SYMBOL_NAME";
-        return sv.token();
-    };
-    parser["COMMENT"] = [](const peg::SemanticValues &sv)
-    {
-        return sv.token();
-    };
 
-    typedef std::tuple<string, string, string> StringTriple;
-
-    parser["SECTION"] = [](const peg::SemanticValues &sv)
-    {
-        std::cout<<"BBBBBBB: "<<sv.token()<<"bbbb\n"<<"size: "<<sv.size()<<"\n"<<flush;
-        StringTriple triple = sv[0].get<StringTriple>();
-        return Section(std::get<0>(triple), std::get<1>(triple));
-    };
-    parser["SECTION_DEFINITION"] = [](const peg::SemanticValues &sv)
-    {
-        cout<<"SECTION_DEFINITION: "<<sv.size()<<"\n";
-        return std::make_tuple(sv[0].get<string>(), sv[1].get<string>(), sv[2].get<string>());
-    };
-    parser["SECTION_VARIABLE"] = [](const peg::SemanticValues &sv)
-    {
-        cout<<"SECTION_VARIABLE: "<<sv.size()<<"\n";
-        return PropertyVariable(sv[0].get<string>(), sv[1].get<string>());
-    };
-    parser["VALUE"] = [](const peg::SemanticValues &sv)
-    {
-        cout<<"VALUE\n";
-        return sv.token();
-    };
-
-    Section section;
-    parser.enable_packrat_parsing();
-    bool ret = parser.parse("[Ahojda : SIID_LSKDFJ]    # asdfasdfasd asdf asdfasdf \n\r    pokus = ahojda\n\r pokus2 = ahojda2", section);
+    shared_ptr<Ast> ast;
+    parser.enable_ast<Ast>();
+    bool ret = parser.parse("[Ahojda : SIID_LSKDFJ]    # asdfasdfasd asdf asdfasdf \n\r", ast);
     if(ret == true)
     {
-        cout<<"asdfasdf\n";
+        pokus(ast);
     }
     else
     {
         cout<<"error";
     }
 
-    cout << "Hello World!" << endl << section;
+    cout << "Hello World!" << endl;
     return 0;
 }
--- test-cpp-peglib.pro
+++ test-cpp-peglib.pro
@@ -1,5 +1,5 @@
 TEMPLATE = app
-CONFIG += console c++11
+CONFIG += console c++17
 CONFIG += thread
 CONFIG -= app_bundle
 CONFIG -= qt
@@ -11,3 +11,4 @@ INCLUDEPATH += $$PWD/../cpp-peglib
 DEPENDPATH += $$PWD/../cpp-peglib
 
 LIBS += -pthread
+QMAKE_CXXFLAGS += -std=c++14
